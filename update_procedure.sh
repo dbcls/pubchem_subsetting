27de251648cf3fe7699e4ab5a0495d3071c78ce3
Yasunori Yamamoto <yy@dbcls.rois.ac.jp>
2021-07-07 22:08:14 +0900

    ChEMBL RDFに含まれているChEMBL→PPubChem Compoundを追加。

--- update_procedure.sh
+++ update_procedure.sh
@@ -2,38 +2,58 @@
 set -euo pipefail
 
 ENDPOINT=https://integbio.jp/rdf/pubchem/sparql
+EBI_ENDPOINT=https://integbio.jp/rdf/mirror/ebi/sparql
 WORK_DIR=/data/yayamamo/pubchem_fdaapproved_neighbours
-OUT_DIR=FDA_CHEBI-cids
+OUT_DIR=FDA_ChEBI-cids
 FINAL_DESTINATION=/data/togosite/rdf
 CURL=/usr/bin/curl
 PERL=/usr/bin/perl
 XARGS=/usr/bin/xargs
+SORT=/usr/bin/sort
 FIND=/usr/bin/find
 RAPPER=/data/yayamamo/local/bin/rapper
 GREP=/usr/bin/grep
 SED=/usr/bin/sed
 TIMESTAMP=$(date -I)
 LIMIT=1000000
+CID_LIST=FDA_ChEBI_cids_${TIMESTAMP}.txt
+CID_FROM_CHEMBL_LIST=cids_from_ChEMBL_${TIMESTAMP}.txt
+CID4SUBSET=cids4subset_${TIMESTAMP}.txt
 
 cd $WORK_DIR
+
+echo "ChEMBLから繋がっているPubChem IDを取得。"
+if [ -e $CID_FROM_CHEMBL_LIST ]; then rm $CID_FROM_CHEMBL_LIST; fi
+CID_TOTAL=$($CURL -sSH "Accept: text/csv" --data-urlencode query@query_03_count.rq $EBI_ENDPOINT | tail -1)
+echo ChEMBL→CID数: $CID_TOTAL
+COUNT=$(expr $CID_TOTAL / $LIMIT)
+for i in $(seq 0 ${COUNT}); do
+  QUERY=$(sed -e "$ a OFFSET ${i}000000 LIMIT ${LIMIT}" query_03.rq)
+  $CURL -sSH "Accept: text/csv" --data-urlencode query="$QUERY" $EBI_ENDPOINT | tail -n +2 >> $CID_FROM_CHEMBL_LIST
+done
+$SED -i -Ee 's/pubchem/rdf/;s,gov,gov/pubchem,;s,nd/,nd/CID,' $CID_FROM_CHEMBL_LIST
+
 echo "FDA承認薬、ChEBI、およびChEMBLにつながるCIDを取得。"
 echo 実行日: ${TIMESTAMP}
-if [ -e FDA_CHEBI_cids_${TIMESTAMP}.txt ]; then rm FDA_CHEBI_cids_${TIMESTAMP}.txt; fi
+if [ -e $CID_LIST ]; then rm $CID_LIST; fi
 CID_TOTAL=$($CURL -sSH "Accept: text/csv" --data-urlencode query@query_01_count.rq $ENDPOINT | tail -1)
 echo 取得対象CID数: $CID_TOTAL
 COUNT=$(expr $CID_TOTAL / $LIMIT)
 for i in $(seq 0 ${COUNT}); do
   QUERY=$(sed -e "$ a OFFSET ${i}000000 LIMIT ${LIMIT}" query_01.rq)
-  $CURL -sSH "Accept: text/csv" --data-urlencode query="$QUERY" $ENDPOINT | tail -n +2 >> FDA_CHEBI_cids_${TIMESTAMP}.txt
+  $CURL -sSH "Accept: text/csv" --data-urlencode query="$QUERY" $ENDPOINT | tail -n +2 >> $CID_LIST
 done
-$SED -i.bak -e '/CID313"/d' FDA_CHEBI_cids_${TIMESTAMP}.txt # HClを除去（関連トリプル数が巨大なため別途特別対応している）
+
+$SORT -u $CID_FROM_CHEMBL_LIST $CID_LIST > $CID4SUBSET
+
+$SED -i.bak -e '/CID313"/d' $CID4SUBSET # HClを除去（関連トリプル数が巨大なため別途特別対応している）
 echo "ATC分類情報を取得。"
 $CURL -sSH "Accept: application/n-triples" -o skos_concept_${TIMESTAMP}.nt --data-urlencode query="CONSTRUCT WHERE { ?attr a <http://www.w3.org/2004/02/skos/core#concept> ; ?p ?o .}" $ENDPOINT
 
 echo "取得したCIDについて、それが主語もしくは目的語になるトリプルのうち、CID同士を結ばないものを取得。"
 if [ -e ${OUT_DIR} ]; then $FIND ${OUT_DIR} -type f -exec rm "{}" \; ; else mkdir ${OUT_DIR} ; fi
 $CURL -sSH "Accept: application/n-triples" -o ${OUT_DIR}/HydrochloricAcid.nt --data-urlencode query@query_02.rq $ENDPOINT
-$PERL ./construct_1.pl ./FDA_CHEBI_cids_${TIMESTAMP}.txt | xargs -0 -i -P10 ./get_describes.sh ${OUT_DIR} "{}" nt
+$PERL ./construct_1.pl $CID4SUBSET | xargs -0 -i -P10 ./get_describes.sh ${OUT_DIR} "{}" nt
 RES=$($FIND ${OUT_DIR} -maxdepth 1 -name *.err 2>/dev/null)
 if [ $? -ne 0 ]; then
   echo "$FIND の実行に失敗しました。"
